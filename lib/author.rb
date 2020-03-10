2a0d923cb44d7f9e0a20b1dcfdaaef515e0260bc
Melissa Sperry <sperry.mel@gmail.com>
2020-03-10 01:42:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,25 @@
 class Author
-
-  attr_accessor :name, :posts
-
-  @@post_count = 0
-
+  attr_accessor :name
+  @@post_count = 1
   def initialize(name)
     @name = name
     @posts = []
   end
-
+  def posts
+    #@posts
+    Post.all.select {|post| post.author == self}
+  end
   def add_post(post)
     @posts << post
     post.author = self
     @@post_count += 1
   end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
     @posts << post
     post.author = self
     @@post_count += 1
   end
-
   def self.post_count
     @@post_count
   end
